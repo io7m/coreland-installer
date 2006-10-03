37c6b19346526a0d2c747701556b6d7434435e46
markzero <markzero@6e7d7e42-f3d6-da11-80d8-00d0b73f4485>
2006-10-03 12:36:21 +0000

    new tools
    git-svn-id: file:///source/coreland/installer/trunk@1097 6e7d7e42-f3d6-da11-80d8-00d0b73f4485

--- UNIT_TESTS/t_inst2h.c
+++ UNIT_TESTS/t_inst2h.c
@@ -4,7 +4,7 @@
 
 struct install_item insthier[] = {
   {INST_MKDIR,   0, 0, TODIR, 0, 0, 0700},
-  {INST_COPY,    "files/t2/t2.lib", 0, TODIR, 0, 0, 0600},
-  {INST_LIBLINK, "files/t2/t2.lib", "libt2", TODIR, 0, 0, 0600},
+  {INST_COPY,    "files/t2/t2.vlb", 0, TODIR, 0, 0, 0600},
+  {INST_LIBLINK, "files/t2/t2.vlb", "libt2", TODIR, 0, 0, 0600},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
